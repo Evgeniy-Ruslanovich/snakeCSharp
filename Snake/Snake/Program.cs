a1908fe98d8dd50702f98dbacdfe6632f1866da7
Evgeniy-Ruslanovich <evgeniy.zaynutdinov@gmail.com>
2018-06-03 21:44:17 +0300

    Создали класс для символа, с полями и методом отрисовки в нужных координатах

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,30 +10,40 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 4;
+            p1.sym = '*';
+            p1.Draw();
+           /* int x1 = 1;
             int y1 = 4;
             char symbol1 = '*';
             //Console.SetCursorPosition(x1, y1);
-            //Console.Write(symbol1);
-            WriteSymbol(x1, y1, symbol1);
+            //Console.Write(symbol1);*/
+           // WriteSymbol(p1.x, p1.y, p1.sym);
 
-            int x2 = 4;
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 2;
+            p2.sym = '#';
+            p2.Draw();
+            /*int x2 = 4;
             int y2 = 2;
             char symbol2 = '#';
             //Console.SetCursorPosition(x2, y2);
-            //Console.Write(symbol2);
-            WriteSymbol(x2, y2, symbol2);
+            //Console.Write(symbol2);*/
+           // WriteSymbol(p2.x, p2.y, p2.sym);
 
             Console.Read();
 
             
         }
 
-        static void WriteSymbol(int x, int y, char symbol)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(symbol);
-        }
+        //static void WriteSymbol(int x, int y, char symbol)
+        //{
+        //    Console.SetCursorPosition(x, y);
+        //    Console.Write(symbol);
+        //}
     }
 
     
