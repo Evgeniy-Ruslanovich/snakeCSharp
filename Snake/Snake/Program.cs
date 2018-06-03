657fbc183d7b17488f6e7f5c2d91322f5753c60c
Evgeniy-Ruslanovich <evgeniy.zaynutdinov@gmail.com>
2018-06-03 22:31:11 +0300

    Убрал все лишнее

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,41 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 4;
-            p1.sym = '*';
+            Point p1 = new Point(1, 4, '*');
             p1.Draw();
-           /* int x1 = 1;
-            int y1 = 4;
-            char symbol1 = '*';
-            //Console.SetCursorPosition(x1, y1);
-            //Console.Write(symbol1);*/
-           // WriteSymbol(p1.x, p1.y, p1.sym);
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 2;
-            p2.sym = '#';
+            Point p2 = new Point(4, 2, '#');
             p2.Draw();
-            /*int x2 = 4;
-            int y2 = 2;
-            char symbol2 = '#';
-            //Console.SetCursorPosition(x2, y2);
-            //Console.Write(symbol2);*/
-           // WriteSymbol(p2.x, p2.y, p2.sym);
+
+            Point p3 = new Point(1,1,'a');
+            p3.Draw();
 
             Console.Read();
 
             
         }
-
-        //static void WriteSymbol(int x, int y, char symbol)
-        //{
-        //    Console.SetCursorPosition(x, y);
-        //    Console.Write(symbol);
-        //}
-    }
-
-    
+    } 
 }
