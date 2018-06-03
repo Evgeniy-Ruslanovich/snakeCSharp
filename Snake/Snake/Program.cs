8815c84a509f9c7f29d454505b761b10f1291f89
Evgeniy-Ruslanovich <evgeniy.zaynutdinov@gmail.com>
2018-06-03 20:40:12 +0300

    Создали функцию WriteSymbol

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,31 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x1 = 1;
+            int y1 = 4;
+            char symbol1 = '*';
+            //Console.SetCursorPosition(x1, y1);
+            //Console.Write(symbol1);
+            WriteSymbol(x1, y1, symbol1);
 
+            int x2 = 4;
+            int y2 = 2;
+            char symbol2 = '#';
+            //Console.SetCursorPosition(x2, y2);
+            //Console.Write(symbol2);
+            WriteSymbol(x2, y2, symbol2);
+
+            Console.Read();
+
+            
+        }
+
+        static void WriteSymbol(int x, int y, char symbol)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
         }
     }
+
+    
 }
