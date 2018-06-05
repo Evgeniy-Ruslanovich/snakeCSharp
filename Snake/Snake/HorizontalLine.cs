7832dc36abec9b955a64e94c51db616c2ee842c7
Evgeniy-Ruslanovich <evgeniy.zaynutdinov@gmail.com>
2018-06-05 19:57:21 +0300

    Добавлен класс "горизонтальная линия"

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        //Конструктор
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            if(xLeft > xRight)
+            {
+                xRight = xLeft;//Защита от бесконечного цикла, если нам правый зададут меньше, чем левый
+            }
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,30 +11,22 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 4, '*');
-            p1.Draw();
 
             Point p2 = new Point(4, 2, '#');
-            p2.Draw();
 
             Point p3 = new Point(1,1,'a');
-            p3.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-            Console.WriteLine(numList[1]);
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(numList[i]);
-            }
-            numList.RemoveAt(0);
 
             List < Point > pList= new List<Point>();
             pList.Add(p1);
             pList.Add(p2);
             pList.Add(p3);
+            foreach (Point point in pList)
+            {
+                point.Draw();
+            }
+
+            HorizontalLine hl1 = new HorizontalLine(5,3,5,'*');
+            hl1.Draw();
 
             Console.Read();
 
